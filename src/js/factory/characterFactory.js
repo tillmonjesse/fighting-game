a6e8e1e3647c60bc3789d476d6948205054faff1
tillmonjesse <48810526+tillmonjesse@users.noreply.github.com>
2020-05-09 21:00:57 -0700

    Moved movement and attack to character physics

--- src/js/factory/characterFactory.js
+++ src/js/factory/characterFactory.js
@@ -1,5 +1,5 @@
 import {WIDTH, HEIGHT} from '../constant.js';
-export const characterCreate = (scene,config) => {
+export const characterCreate = (scene, config, cursors, punchDirection) => {
 	let character = scene.physics.add.image(
 		config.x || 0, 
 		config.y || 0,
@@ -23,9 +23,19 @@ export const characterCreate = (scene,config) => {
     	animation: 0
     };
     character.hitDamage = hitDamage;
+    character.characterAttack = characterAttack;
+    character.punch = scene.add.ellipse(WIDTH + 300, HEIGHT + 300, 100, 100, 0xff0000, 1);
+    scene.physics.add.existing(character.punch);
+    character.punch.body.setAllowDrag(false);
+    character.punch.body.setAllowGravity(false);
+    character.punch.body.setAllowRotation(false);
+    character.punch.body.setMaxVelocity(0);
+    character.updateMovement = updateMovement;
+    character.cursors = cursors;
+    character.punchDirection = punchDirection;
     return character;
 };
-function hitAnimation(punchKey, punch, enemy) {
+function hitAnimation(punchKey, enemy) {
    punchKey.sinceFirstPressed++;
     if (punchKey.sinceFirstPressed > this.hit.lightAttack && punchKey.sinceFirstPressed <= this.hit.heavyAttack)
     {
@@ -35,7 +45,7 @@ function hitAnimation(punchKey, punch, enemy) {
             this.hitDamage(this.hit.lightDamage, enemy);
             punchKey.sinceFirstPressed = 0;
             this.attack.animation = 0;
-            punch.setPosition(WIDTH + 100, HEIGHT + 100);
+            this.punch.setPosition(WIDTH + 100, HEIGHT + 100);
             console.log('light attack finished');
             return false;
         }
@@ -49,7 +59,7 @@ function hitAnimation(punchKey, punch, enemy) {
             this.hitDamage(this.hit.heavyDamage, enemy);
             punchKey.sinceFirstPressed = 0;
             this.attack.animation = 0;
-            punch.setPosition(WIDTH + 100, HEIGHT + 100);
+            this.punch.setPosition(WIDTH + 100, HEIGHT + 100);
             console.log('heavy attack finished');
             return false;
         }
@@ -69,4 +79,72 @@ function hitDamage(hitValue, enemy) {
     }
     this.hit.detected = 0;
     console.log(enemy.health);
+}
+function characterAttack(enemy) {
+	if (this.punchDirection.punchRight.isDown) 
+    {
+        this.punchDirection.punchRight.sinceFirstPressed++;
+    }
+	else if (this.punchDirection.punchRight.isUp && this.punchDirection.punchRight.sinceFirstPressed > 0) 
+    {
+        if (this.hitAnimation(this.punchDirection.punchRight, enemy)) {
+            this.punch.setPosition(this.x + 50, this.y);
+        }
+    }
+    else if (this.punchDirection.punchLeft.isDown)
+     {
+        this.punchDirection.punchLeft.sinceFirstPressed++;
+    }
+    else if (this.punchDirection.punchLeft.isUp && this.punchDirection.punchLeft.sinceFirstPressed > 0) 
+    {
+        if (this.hitAnimation(this.punchDirection.punchLeft, enemy)) {
+            this.punch.setPosition(this.x + -50, this.y);
+        }
+    }
+    else if (this.punchDirection.punchUp.isDown) 
+    {
+        this.punchDirection.punchUp.sinceFirstPressed++;
+    }
+    else if (this.punchDirection.punchUp.isUp && this.punchDirection.punchUp.sinceFirstPressed > 0) 
+    {
+        if (this.hitAnimation(this.punchDirection.punchUp, enemy)) {
+            this.punch.setPosition(this.x, this.y + -50);
+        }
+    }
+    else if (this.punchDirection.punchDown.isDown) 
+    {
+        this.punchDirection.punchDown.sinceFirstPressed++;
+    }
+    else if (this.punchDirection.punchDown.isUp && this.punchDirection.punchDown.sinceFirstPressed > 0) 
+    {
+        if (this.hitAnimation(this.punchDirection.punchDown, enemy)) {
+            this.punch.setPosition(this.x, this.y + 50);
+        }
+    }
+}
+function updateMovement () {
+	if (this.cursors.right.isDown)
+    {
+        this.setAccelerationX(200);
+    }
+    else if (this.cursors.left.isDown)
+    {
+        this.setAccelerationX(-200);
+    }
+    else
+    {
+        this.setAccelerationX(0);
+    }
+    if (this.cursors.up.isDown && this.body.touching.down)
+    {
+        this.setVelocityY(-200);
+    }
+    else if (this.cursors.down.isDown)
+    {
+        this.setAccelerationY(200);
+    }
+    else
+    {
+        this.setAccelerationY(0);
+    }
 }
--- src/js/scene/initial.js
+++ src/js/scene/initial.js
@@ -5,25 +5,9 @@ const scene = {
     preload, create, update
 };
 export default scene;
-let punchRight;
-let punchLeft;
-let punchUp;
-let punchDown;
-let cursors;
 let platforms;
 let logo;
 let enemy;
-let punch;
-const hit = {
-    lightAttack: 7.5,
-    heavyAttack: 30,
-    lightDamage: 7.5,
-    heavyDamage: 30,
-    detected: 0
-};
-const attack = {
-    animation: 0
-};
 function preload ()
 {
 
@@ -37,17 +21,19 @@ function preload ()
 
 function create ()
 {
-    punchRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    punchRight.sinceFirstPressed = 0;
-    punchLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-    punchLeft.sinceFirstPressed = 0;
-    punchUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-    punchUp.sinceFirstPressed = 0;
-    punchDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    punchDown.sinceFirstPressed = 0;
+    var punchDirection = {
+        punchRight: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+        punchLeft: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+        punchUp: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+        punchDown: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+        
+    }
+    punchDirection.punchRight.sinceFirstPressed = 0;
+    punchDirection.punchLeft.sinceFirstPressed = 0;
+    punchDirection.punchUp.sinceFirstPressed = 0;
+    punchDirection.punchDown.sinceFirstPressed = 0;
 
     this.add.image(WIDTH/2, HEIGHT/2, 'sky');
-    cursors = this.input.keyboard.createCursorKeys();
     platforms = this.physics.add.staticGroup();
 
     platforms.create(WIDTH/2, HEIGHT, 'ground');
@@ -59,10 +45,15 @@ function create ()
         scale: { start: 1, end: 0 },
         blendMode: 'ADD'
     });
-    logo = characterCreate(this, {
-        asset: 'logo',
-        health: 100
-    });
+    logo = characterCreate(
+        this, 
+        {
+            asset: 'logo',
+            health: 100
+        },
+        this.input.keyboard.createCursorKeys(),
+        punchDirection
+    );
 
     enemy = characterCreate(this, {
         x: WIDTH/2,
@@ -71,21 +62,13 @@ function create ()
         health: 100
     });
 
-    punch = this.add.ellipse(WIDTH + 300, HEIGHT + 300, 100, 100, 0xff0000, 1);
-
-    this.physics.add.existing(punch);
-
-    punch.body.setAllowDrag(false);
-    punch.body.setAllowGravity(false);
-    punch.body.setAllowRotation(false);
-    punch.body.setMaxVelocity(0);
     
     logo.setCollideWorldBounds(true);
     enemy.setCollideWorldBounds(true);
     this.physics.add.collider(logo, platforms);
     this.physics.add.collider(enemy, platforms);
     this.physics.add.collider(logo, enemy);
-    this.physics.add.overlap(punch, enemy, function (punch, enemy)
+    this.physics.add.overlap(logo.punch, enemy, function (punch, enemy)
     {
     	++logo.hit.detected;
     });
@@ -94,68 +77,6 @@ function create ()
 }
 function update ()
 {
-    if (cursors.right.isDown)
-    {
-        this.physics.velocityFromRotation(0, 200, logo.body.acceleration);
-    }
-    else if (cursors.left.isDown)
-    {
-        this.physics.velocityFromRotation(180, 200, logo.body.acceleration);
-    }
-    else
-    {
-        logo.setAccelerationX(0);
-    }
-    if (cursors.up.isDown && logo.body.touching.down)
-    {
-        logo.setVelocityY(-200);
-    }
-    else if (cursors.down.isDown)
-    {
-        logo.setAccelerationY(200);
-    }
-    else
-    {
-        logo.setAccelerationY(0);
-    }
-    if (punchRight.isDown) 
-    {
-        punchRight.sinceFirstPressed++;
-    }
-    else if (punchRight.isUp && punchRight.sinceFirstPressed > 0) 
-    {
-        if (logo.hitAnimation(punchRight, punch, enemy)) {
-            punch.setPosition(logo.x + 50, logo.y);
-        }
-    }
-    else if (punchLeft.isDown)
-     {
-        punchLeft.sinceFirstPressed++;
-    }
-    else if (punchLeft.isUp && punchLeft.sinceFirstPressed > 0) 
-    {
-        if (logo.hitAnimation(punchLeft, punch, enemy)) {
-            punch.setPosition(logo.x + -50, logo.y);
-        }
-    }
-    else if (punchUp.isDown) 
-    {
-        punchUp.sinceFirstPressed++;
-    }
-    else if (punchUp.isUp && punchUp.sinceFirstPressed > 0) 
-    {
-        if (logo.hitAnimation(punchUp, punch, enemy)) {
-            punch.setPosition(logo.x, logo.y + -50);
-        }
-    }
-    else if (punchDown.isDown) 
-    {
-        punchDown.sinceFirstPressed++;
-    }
-    else if (punchDown.isUp && punchDown.sinceFirstPressed > 0) 
-    {
-        if (logo.hitAnimation(punchDown, punch, enemy)) {
-            punch.setPosition(logo.x, logo.y + 50);
-        }
-    }
+    logo.updateMovement();
+    logo.characterAttack(enemy);
 }
