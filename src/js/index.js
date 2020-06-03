c4c6276869d8e42adc5dac5144cc1bfa5403ac4a
tillmonjesse <48810526+tillmonjesse@users.noreply.github.com>
2020-06-02 20:02:34 -0700

    installed react

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import config from './config.js';
 import Phaser from 'phaser';
-document.getElementById('gravity').value = config.physics.arcade.gravity.y;
+/*document.getElementById('gravity').value = config.physics.arcade.gravity.y;
 function gravityChange(input) {
     console.log(input.valueAsNumber);
     game.scene.scenes[0].physics.world.gravity.y = input.valueAsNumber;
@@ -9,6 +9,6 @@ document.getElementById('width').value = config.width;
 function widthChange(input) {
     console.log(input.valueAsNumber);
     game.config.width = input.valueAsNumber;
-}
+}*/
 
 var game = new Phaser.Game(config);
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,34 @@
 const path = require('path');
 
 module.exports = {
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
 	mode: 'development',
-  entry: './src/js/index.js',
+  entry: {
+    main: './src/js/index.js',
+    tooling: './src/tooling/index.jsx'
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
   devServer: {
   	contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+          /*options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }*/
+        }
+      }
+    ]
   }
 };
